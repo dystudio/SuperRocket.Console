f776e8450864e86996d583094a1c0d27abf8c047
david0718 <david0718@qq.com>
2019-09-04 14:43:14 +0800

    add easynetq support

--- SuperRocket.Console/Program.cs
+++ SuperRocket.Console/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Abp;
 using Abp.Dependency;
 using Castle.Facilities.Logging;
+using EasyNetQ;
+using EasyNetQ.NonGeneric;
+using Messages;
 
 namespace AbpEfConsoleApp
 {
@@ -18,6 +22,32 @@ namespace AbpEfConsoleApp
 
                 bootstrapper.Initialize();
 
+                using (var bus = RabbitHutch.CreateBus("host=localhost"))
+                {
+                    bus.SubscribeAsync<TextMessage>("test", message => Task.Factory.StartNew(() =>
+                    {
+                        // Perform some actions here
+                        // If there is a exception it will result in a task complete but task faulted which
+                        // is dealt with below in the continuation
+                    }).ContinueWith(task =>
+                    {
+                        if (task.IsCompleted && !task.IsFaulted)
+                        {
+                            // Everything worked out ok
+                            HandleTextMessage(message);
+                        }
+                        else
+                        {
+                            // Don't catch this, it is caught further up the hierarchy and results in being sent to the default error queue
+                            // on the broker
+                            throw new EasyNetQException("Message processing exception - look in the default error queue (broker)");
+                        }
+                    }));
+
+                    Console.WriteLine("Listening for messages. Hit <return> to quit.");
+                    Console.ReadLine();
+                }
+
                 //Getting a Tester object from DI and running it
                 using (var tester = bootstrapper.IocManager.ResolveAsDisposable<Tester>())
                 {
@@ -27,6 +57,13 @@ namespace AbpEfConsoleApp
                 Console.WriteLine("Press enter to exit...");
                 Console.ReadLine();
             }
+
+        }
+
+        static void HandleTextMessage(TextMessage textMessage)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Got message {textMessage.Text}");
         }
     }
 }
--- SuperRocket.Console/Tester.cs
+++ SuperRocket.Console/Tester.cs
@@ -38,19 +38,19 @@ namespace AbpEfConsoleApp
             _backgroundJobManager.Enqueue<TestJob, int>(1);
 
             //GetAllList
-            foreach (var user in _userRepository.GetAllList())
-            {
-                Console.WriteLine(user);
-            }
+            //foreach (var user in _userRepository.GetAllList())
+            //{
+            //    Console.WriteLine(user);
+            //}
 
-            //Get
-            Console.WriteLine("Halil: " + _userRepository.Get(new Guid("c2ee8f4e-8592-44d5-84c2-ac5fca1752fd")));
+            ////Get
+            //Console.WriteLine("Halil: " + _userRepository.Get(new Guid("c2ee8f4e-8592-44d5-84c2-ac5fca1752fd")));
 
-            //FirstOrDefault
-            Console.WriteLine("Emre: " + _userRepository.FirstOrDefault(new Guid("b7f88a8e-736e-4708-87d5-beab34f1533b")));
+            ////FirstOrDefault
+            //Console.WriteLine("Emre: " + _userRepository.FirstOrDefault(new Guid("b7f88a8e-736e-4708-87d5-beab34f1533b")));
 
-            //Unknown user
-            Console.WriteLine("null! " + _userRepository.FirstOrDefault(Guid.NewGuid()));
+            ////Unknown user
+            //Console.WriteLine("null! " + _userRepository.FirstOrDefault(Guid.NewGuid()));
 
             Logger.Debug("Finished Tester.Run()");
         }
